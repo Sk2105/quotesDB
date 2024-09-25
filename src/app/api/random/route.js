224d901beb442d08d7f73bf1a48784c202e3f0b7
sachin kumar <sk2105@yahoo.com>
2024-09-25 18:56:31 +0530

    random issue solve

--- src/app/api/random/route.js
+++ src/app/api/random/route.js
@@ -1,16 +1,19 @@
 import { NextResponse } from "next/server";
 import db from "@/database/dbConnection";
+/**
+ * Returns a random quote from the database.
+ * @returns {NextResponse} A JSON response with a random quote
+ */
 export async function GET() {
   try {
     const quotes = async () => {
-      return await db.sql`use database QuotesDB; select id,quote,(select author from authors where id=authorId) as author,(select tag from Tags where id=tagId) as category from quotes`;
+      return await db.sql`use database QuotesDB; select id,quote,(select author from authors where id=authorId) as author,(select tag from Tags where id=tagId) as category from quotes where id = (select ABS(random()) % (select count(*) from quotes) + 1)`;
     };
-    const data = await quotes();
-    const random = Math.floor(Math.random() * data.length);
-    const result = data[random];
-    result.author = result.author === null ? "Anonymous" : result.author;
-    result.category = result.category === null ? "General" : result.category;
-    return NextResponse.json(result);
+    const result = await quotes();
+    const quote = result[0];
+    quote.author = quote.author === null ? "Anonymous" : quote.author;
+    quote.category = quote.category === null ? "General" : quote.category;
+    return NextResponse.json(quote);
   } catch (error) {
     return NextResponse.json({ error: error.message });
   }
